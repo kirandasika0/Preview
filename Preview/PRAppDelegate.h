35accdfda2b9e622c825d434cedb76ac1e0ea15c
SaiKiran Dasika <Kiran@SaiKirans-MacBook-Air.local>
2014-05-10 14:22:52 +0530

    Initial Commit

---
+++ Preview/PRAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  PRAppDelegate.h
+//  Preview
+//
+//  Created by SaiKiran Dasika on 10/05/14.
+//  Copyright (c) 2014 Burst. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PRAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Preview/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Preview
+//
+//  Created by SaiKiran Dasika on 10/05/14.
+//  Copyright (c) 2014 Burst. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "PRAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([PRAppDelegate class]));
+    }
+}
